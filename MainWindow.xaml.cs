4b4b8bb7b9e79bf6668089b2b557021fa32e5b4f
douglasvinicio <douglas.vinicio@hotmail.com>
2021-01-10 20:13:32 -0800

    Temperature Converter Final

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,12 +22,26 @@ namespace TempConv
     {
         public MainWindow()
         {
+            
             InitializeComponent();
         }
 
-        private void TempCovert()
+        private void rbtn_Checked(object sender, RoutedEventArgs e)
+        {
+            // I need a method to be called to convert the temperature
+            TempConvert();
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TempConvert();
+        }
+
+
+        private void TempConvert()
         {
             double temp;
+
 
             if (!double.TryParse(txtInput.Text,out temp))
             {
@@ -38,14 +52,17 @@ namespace TempConv
             {
                 if (rbtnCelsiusOutput.IsChecked == true)
                 {
+                    // Do not convert just show the same
                     result.Content = string.Format("{0:0.0} C", temp);
                 }
                 if (rbtnFarenheintOutput.IsChecked == true)
                 {
+                    // Convert from Celsius to Far
                     result.Content = string.Format("{0:0.0} F", ((temp * 9 / 5) + 32));
                 }
                 else if (rbtnKelvinOutput.IsChecked == true)
                 {
+                    // Convert from Celsius to Kelvin
                     result.Content = string.Format("{0:0.0} K", (temp + 237.15));
                 }
             }
@@ -54,19 +71,45 @@ namespace TempConv
             {
                 if (rbtnCelsiusOutput.IsChecked == true)
                 {
-                    
+                    // Convert from Far to Cel
+                    // Celsius = (Far - 32) / 1.8
+                    result.Content = string.Format("{0:0.0} C", (temp - 32) / 1.8);
                 }
 
                 else if (rbtnKelvinOutput.IsChecked == true)
                 {
+                    // Convert from Far to Kelvin 
+                    // Kelvin (K) = (Fahrenheit - 32) / 1.8 + 273.15
+                    result.Content = string.Format("{0:0.0} C", (temp - 32) / 1.8 + 273.15);
+                }
 
+                else if (rbtnFarenheintOutput.IsChecked == true)
+                {
+                    // Do not convert
+                    result.Content = string.Format("{0:0.0}", temp);
                 }
             }
-        }
 
-        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
-        {
-
+            if (rbtnKelvinInput.IsChecked == true)
+            {
+                if (rbtnCelsiusOutput.IsChecked == true)
+                {
+                    // Convert from Kelvin to Celsius
+                    // Celsius (°C) = Kelvin - 273.15
+                    result.Content = string.Format("{0:0.0} C", temp - 273.15);
+                }
+                else if (rbtnFarenheintOutput.IsChecked == true)
+                {
+                    // Convert from Kelvin to Farenheint
+                    // Fahrenheit (°F) = Kelvin x 1.8 - 459.67
+                    result.Content = string.Format("{0:0.0} C", temp * 1.8 - 459.67);
+                }
+                else if (rbtnKelvinOutput.IsChecked == true)
+                {
+                    // Do not convert just show the same amount
+                    result.Content = string.Format("{0:0.0}", temp);
+                }
+            }
         }
     }
 }
